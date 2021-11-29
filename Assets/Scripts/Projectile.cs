8d65c93e6346346cc54d64ff68e2749e80c24073
MimiRaven <mtorres99@Knights.ucf.edu>
2021-11-29 11:37:34 -0500

    Challenge commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+
+        HardEnemyController E = other.collider.GetComponent<HardEnemyController>();
+        if (E != null)
+        {
+            E.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
+    public GameObject projectilePrefab;
+    public GameObject healthincreasePrefab;
+    public GameObject hiteffectPrefab;
+
+    public int score;
+
+    public Text scoreText;
+    public Text winText;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    Animator animator;
+    AudioSource audioSource;
+
+    Vector2 lookDirection = new Vector2(1,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        score = 0;
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            animator.SetTrigger("Hit");
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            PlaySound(hitSound);
+            GameObject hiteffectObject = Instantiate(hiteffectPrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        }
+
+        if (amount > 0)
+        {
+            GameObject healthincreaseObject = Instantiate(healthincreasePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+
+    public void ChangeScore(int scoreAmount)
+    {
+        score += 1;
+        scoreText.text = "Fixed Robots: " + score.ToString();
+
+        if (score >= 4)
+        {
+            winText.text = "You Win! Game Created By: Mia Torres";
+        }
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+        PlaySound(throwSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
